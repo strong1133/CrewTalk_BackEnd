6b4804c42463ab136841ce0b3a40e1ae17b575ae
sanyoni <ksyline1997@gmail.com>
2021-04-13 14:58:39 +0900

    feat: JwtFilter를 적용하도록 추가하는 Config클래스 작성

---
+++ crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/security/jwt/JwtSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.hh99_crewtalk.crewtalk.security.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@RequiredArgsConstructor
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final JwtTokenProvider jwtProvider;
+
+    @Override
+    public void configure(HttpSecurity builder) {
+        JwtFilter jwtFilter = new JwtFilter(jwtProvider);
+
+        builder.addFilter(jwtFilter);
+    }
+}
