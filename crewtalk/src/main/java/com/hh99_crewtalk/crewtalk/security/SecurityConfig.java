595d09a4dafb60ad8507b8a6766ffe9904d4a30a
sanyoni <ksyline1997@gmail.com>
2021-04-13 15:33:13 +0900

    feat: Spring Security의 적용을 받지 않는 주소 설정

--- crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/security/SecurityConfig.java
+++ crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/security/SecurityConfig.java
@@ -1,9 +1,22 @@
 package com.hh99_crewtalk.crewtalk.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                // Spring Security가 무시할 요청의 주소들을 추가합니다.
+                .ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .antMatchers(
+                        "/h2-console/**"
+                        , "/favicon.ico"
+                );
+    }
 }
