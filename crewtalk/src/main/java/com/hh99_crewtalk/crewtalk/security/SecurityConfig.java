1a69b34b277051b318bfa35ead8d3095865f634d
sanyoni <ksyline1997@gmail.com>
2021-04-13 20:36:47 +0900

    fix: PasswordEncoder를 스프링으로부터 주입받을 수 있도록 수정

--- crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/security/SecurityConfig.java
+++ crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/security/SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.hh99_crewtalk.crewtalk.security;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void configure(WebSecurity web) {
