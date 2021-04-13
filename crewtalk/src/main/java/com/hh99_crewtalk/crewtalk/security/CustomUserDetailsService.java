d19871c37d3ce9e7e63d064203eb58900a5faf06
sanyoni <ksyline1997@gmail.com>
2021-04-13 20:27:34 +0900

    fix: 스프링이 우리가 만든 CustomUserDetails를 사용하지 않던 문제 수정

--- crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/security/CustomUserDetailsService.java
+++ crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/security/CustomUserDetailsService.java
@@ -6,8 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
