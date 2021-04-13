ff1f4e9e666750a02c24f4806aa8096659eaa101
sanyoni <ksyline1997@gmail.com>
2021-04-13 20:27:03 +0900

    fix: user signup할 때 평문 password를 그대로 저장하지 않고 암호화된 password를 저장하도록 수정

--- crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/domain/Member.java
+++ crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/domain/Member.java
@@ -3,15 +3,15 @@ package com.hh99_crewtalk.crewtalk.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.hh99_crewtalk.crewtalk.dto.MemberUpdateDto;
 import com.hh99_crewtalk.crewtalk.dto.SignupRequestDto;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @Entity
-public class Member extends Timestamped{
+public class Member extends Timestamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -27,14 +27,14 @@ public class Member extends Timestamped{
     @Column(nullable = false)
     private String stack;
 
-    public Member(SignupRequestDto signupRequestDto){
+    public Member(SignupRequestDto signupRequestDto) {
         this.userId = signupRequestDto.getUserId();
         this.password = signupRequestDto.getPassword();
         this.stack = signupRequestDto.getStack();
     }
 
     // 유저정보 수정
-    public void updateMember(MemberUpdateDto memberUpdateDto){
+    public void updateMember(MemberUpdateDto memberUpdateDto) {
         this.userId = memberUpdateDto.getUserId();
         this.stack = memberUpdateDto.getStack();
     }
--- crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/service/MemberService.java
+++ crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/service/MemberService.java
@@ -1,12 +1,12 @@
 package com.hh99_crewtalk.crewtalk.service;
 
 
-
 import com.hh99_crewtalk.crewtalk.domain.Member;
 import com.hh99_crewtalk.crewtalk.dto.MemberUpdateDto;
 import com.hh99_crewtalk.crewtalk.dto.SignupRequestDto;
 import com.hh99_crewtalk.crewtalk.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -17,31 +17,36 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     //전체 유저 조회
     @Transactional
-    public List<Member> findAllUser(){
+    public List<Member> findAllUser() {
         return memberRepository.findAll();
     }
 
     // 스택별 유저 조회
     @Transactional
-    public List<Member> findAllUserByStack(String stack){
+    public List<Member> findAllUserByStack(String stack) {
         return memberRepository.findAllByStack(stack);
     }
 
     //회원가입
     @Transactional
-    public Member createUser(SignupRequestDto signupRequestDto){
+    public Member createUser(SignupRequestDto signupRequestDto) {
+        String encryptedPassword = passwordEncoder.encode(signupRequestDto.getPassword());
+
         Member member = new Member(signupRequestDto);
+        member.setPassword(encryptedPassword);
+
         return memberRepository.save(member);
     }
 
     //유저 정보 변경
     @Transactional
-    public Long updateMember(Long id, MemberUpdateDto memberUpdateDto){
+    public Long updateMember(Long id, MemberUpdateDto memberUpdateDto) {
         Member member = memberRepository.findById(id).orElseThrow(
-                ()-> new IllegalArgumentException("해당 아이디가 없습니다.")
+                () -> new IllegalArgumentException("해당 아이디가 없습니다.")
         );
         member.updateMember(memberUpdateDto);
         return member.getId();
