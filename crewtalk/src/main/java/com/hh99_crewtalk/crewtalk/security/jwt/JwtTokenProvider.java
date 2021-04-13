9b8a9e66332533cda84d8177337861f14ea6c7d3
sanyoni <ksyline1997@gmail.com>
2021-04-13 11:32:15 +0900

    feat: Authentication으로 JwtToken을 만드는 기능 구현

--- crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/security/jwt/JwtTokenProvider.java
+++ crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/security/jwt/JwtTokenProvider.java
@@ -1,18 +1,42 @@
 package com.hh99_crewtalk.crewtalk.security.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
     private final String secret;
     private final long tokenValidityInMilliseconds;
 
+    private Key key;
+
     public JwtTokenProvider(
             @Value("${jwt.secret}") String secret,
             @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds) {
         this.secret = secret;
         this.tokenValidityInMilliseconds = tokenValidityInSeconds * 1000;
+
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String createToken(Authentication authentication) {
+        long now = (new Date()).getTime();
+        Date validity = new Date(now + this.tokenValidityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(validity)
+                .compact();
     }
 
 }
