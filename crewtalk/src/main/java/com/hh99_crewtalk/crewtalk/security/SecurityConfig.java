fcef4cc93160d4a10070c921eb73636768901b56
sanyoni <ksyline1997@gmail.com>
2021-04-13 15:18:44 +0900

    add: SecurityConfig클래스 추가

---
+++ crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/security/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.hh99_crewtalk.crewtalk.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
