f506370ce4ba648eb3d1dbb4d6d962b0c5267d27
sanyoni <ksyline1997@gmail.com>
2021-04-13 13:56:14 +0900

    feat: Token으로부터 User정보를 얻어내는 메소드 작성

--- crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/security/jwt/JwtTokenProvider.java
+++ crewtalk/src/main/java/com/hh99_crewtalk/crewtalk/security/jwt/JwtTokenProvider.java
@@ -1,14 +1,18 @@
 package com.hh99_crewtalk.crewtalk.security.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.ArrayList;
 import java.util.Date;
 
 @Component
@@ -37,6 +41,22 @@ public class JwtTokenProvider {
                 .signWith(key, SignatureAlgorithm.HS512)
                 .setExpiration(validity)
                 .compact();
+    }
+
+    public Authentication parseToken(String token) {
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        ArrayList<GrantedAuthority> emptyAuthorities = new ArrayList<>(0);
+
+        // token으로부터 user정보를 가져올 때에는 비밀번호를 사용하지 않습니다.
+        // 오로지 token에 있는 userId만으로 user를 가져옵니다.
+        String userId = claims.getSubject();
+        return new UsernamePasswordAuthenticationToken(userId, null, emptyAuthorities);
     }
 
 }
